448afa6560f3c0b609e3497152378a901e7deb35
NathanRF <nathanseribeiro@hotmail.com>
2023-05-14 12:07:39 -0300

    Add project files.

---
+++ dapper-console-example/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Dapper;
+using DapperConsoleExample;
+using System.Data;
+using System.Data.SqlClient;
+
+const string connectionString = "Server=localhost;Database=AdventureWorksLT2019;Trusted_Connection=True;";
+
+await using (SqlConnection connection = new(connectionString))
+{
+  connection.Open();
+  var customers = connection.Query<Customer>("select * from SalesLT.Customer (nolock)");
+  foreach (var customer in customers)
+  {
+    Console.WriteLine(customer.FirstName + " - " + customer.EmailAddress);
+  }
+}
